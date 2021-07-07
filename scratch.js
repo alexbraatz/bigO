1df347007f54996ff472bef05d28713d9eb4c6fb
alexbraatz <alexjbraatz@gmail.com>
2021-07-07 14:56:05 -0500

    main

---
+++ scratch.js
@@ -0,0 +1,7 @@
+// A typical example, O(n^2), which is usually pronounced "Big O squared"
+// The letter "n" represents the input size, and the function "g(n) = n^2"
+// inside the "O()" gives an idea of how complex the alogrithm is with
+// respect to the input size
+
+
+
