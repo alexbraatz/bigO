1bb3d364d82eb2a9eab298f71935bb3e8ba0a45a
alexbraatz <alexjbraatz@gmail.com>
2021-07-07 14:59:00 -0500

    updates

--- scratch.js
+++ scratch.js
@@ -1,7 +1,13 @@
 // A typical example, O(n^2), which is usually pronounced "Big O squared"
 // The letter "n" represents the input size, and the function "g(n) = n^2"
-// inside the "O()" gives an idea of how complex the alogrithm is with
+// inside the "O()" gives an idea of how complex the alogrithm is with 
 // respect to the input size
+
+// A typical algorithm with O(n^2) would be the selection sort algorithm
+// Selection sort is a sorting algorithm that iterates thru the list to ensure
+// every element at index i is the ith smallest/largest element of the list
+
+
 
 
 
