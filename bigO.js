20f265c33865a957ceab89a32fe196e6d44b83d1
alexbraatz <alexjbraatz@gmail.com>
2021-07-08 23:12:49 -0500

    add order n ex

--- bigO.js
+++ bigO.js
@@ -13,3 +13,25 @@ const getLast = items => items[items.Length-1]
 getLast(['a', 'b', 'c', 'd']); //d (1 iteration)
 
 getLast(['a', 'b', 'c', 'd', 'e', 'f', 'g']); //g (1 iteration)
+
+
+// <<--- Linear-Time Algorithm ---->>
+// O(N) - "Order N"
+// Here, the worst case time (iterations) grows on par with number of items
+// i.e. for N elements, you will require N iterations
+
+// ex. code
+const findIndex = (items, match) => {
+  for ( let i = 0, total = items.length; i < total; i++)
+    if ( items[i] == match)
+      return i;
+    
+  return -1; 
+}
+
+// ex. case
+const array = ['a', 'b', 'c', 'd'];
+
+findIndex(array, 'a'); // 0 (1 iteration - best case)
+findIndex(array, 'd') // 3 (4 iterations - worst case)
+findIndex(array, 'e') // -1 (4 iterations - worst case)
