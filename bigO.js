eeb746b65f6a9f1e498364057ac3f148406f6def
alexbraatz <alexjbraatz@gmail.com>
2021-07-08 23:36:48 -0500

    add order n log n ex

--- bigO.js
+++ bigO.js
@@ -66,3 +66,43 @@ console.log( buildSquareMatrix(['a','b', 'c']) ); // 9 iterations, 3 elements re
   ['a','b', 'c'],
 ]
 */
+
+// <<--- Quadratic-Time Algorithm ---->>
+// O(n log n) - "Order N Log N"
+// The holy grail of search/sort algorithms, they're usually the most
+// efficient approach when dealing with large collections. Instead of
+// looking through the components one by one, they split the data in chunks
+// and discard a large amount on every iteation. Usually half, or log base 2
+
+// Assuming you're using a log base 2, one could ideally find a specific
+// element in a collection of one million elements using less than 20 
+// iterations if we scale the size of the collection to a billion, less than 30
+
+// The most popular of this algorithms is the Quicksort. Another is Merge-Sort
+
+// ex. code
+const quickSort = list => {
+  if (list.length < 2)
+    return list;
+  
+  let pivot = list[0];
+  let left = [];
+  let right = [];
+
+  for ( let i = 1, total = list.length; i < total; i++ ){
+    if ( list[i] < pivot )
+      left.push(list[i]);
+    else
+      right.push(list[i])
+  }
+
+  return [
+    ...quickSort(left),
+    pivot,
+    ...quickSort(right)
+  ];
+};
+
+// ex. case
+console.log( quickSort( ['q','a','z','w','s','x','e','d','c','r']) );
+// ["a", "c", "d", "e", "q", "r", "s", "w", "x", "z"]
